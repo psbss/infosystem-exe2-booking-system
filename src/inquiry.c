f8a9767dc1a0fb3f3a802d0ad5a19e9c25c47118
Yuu <uypsbss@gmail.com>
2020-11-10 13:03:41 +0900

    add print message / validate task

--- src/inquiry.c
+++ src/inquiry.c
@@ -8,13 +8,27 @@ int inquiry(void){
 
 	// template
 	// Must Validate
-	printf("診察部位");
+	printf("診察部位\n\
+					頭 : 1\n\
+					手足 : 2\n\
+					胸部, 腹部 : 3\n\
+					予防接種 : 4\n\
+					予約キャンセル : 5\n\
+				");
 	scanf("%d", &issue_part);
-	printf("診察希望日時");
+	// Validate int 1-5
+	printf("診察希望日時(YYYYMMDD)\n\
+					例 : 2020年12月31日 -> 20201231\n\
+				");
 	scanf("%d", &date);
-	printf("午前・午後");
-	scanf("%d", &ampm);
+	// Validate int today-1week
 
-	printf("%d, %d, %d", issue_part, date, ampm);
+	printf("午前 : 1\n\
+					午後 : 2\n\
+				");
+	scanf("%d", &ampm);
+	// Validate int 1-2
+
+	// printf("%d, %d, %d", issue_part, date, ampm);
 	return (0);
 }
