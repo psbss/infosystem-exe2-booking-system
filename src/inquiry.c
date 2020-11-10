3649532d3ca705c60df12b8c2bfdd15812378707
Yuu <uypsbss@gmail.com>
2020-11-10 20:16:34 +0900

    add int validation

--- src/inquiry.c
+++ src/inquiry.c
@@ -1,34 +1,60 @@
 #include "mylib.h"
 
-int inquiry(void){
+int	inquiry(void)
+{
 	// int patient_no;
 	int issue_part;
 	int date;
 	int ampm;
 
-	// template
-	// Must Validate
-	printf("診察部位\n\
-					頭 : 1\n\
-					手足 : 2\n\
-					胸部, 腹部 : 3\n\
-					予防接種 : 4\n\
-					予約キャンセル : 5\n\
-				");
-	scanf("%d", &issue_part);
-	// Validate int 1-5
-	printf("診察希望日時(YYYYMMDD)\n\
-					例 : 2020年12月31日 -> 20201231\n\
-				");
-	scanf("%d", &date);
-	// Validate int today-1week
+	// !! Must Validate !!
+	while (1)
+	{
+		printf("\n診察部位\n頭 : 1\n手足 : 2\n胸部, 腹部 : 3\n予防接種 : 4\n予約キャンセル : 5\n");
+		scanf("%d", &issue_part);
+		// Validate int 1-5
+		if (validate_int(1, 5, issue_part))
+			break;
+		print_validation_err(issue_part);
+	}
 
-	printf("午前 : 1\n\
-					午後 : 2\n\
-				");
-	scanf("%d", &ampm);
-	// Validate int 1-2
+	while (1)
+	{
+		printf("\n診察希望日時(YYYYMMDD)\n例 : 2020年12月31日 -> 20201231\n");
+		scanf("%d", &date);
+		// Validate int today-1week
+		if (validate_date(7, date))
+			break;
+		print_validation_err(date);
+	}
 
-	// printf("%d, %d, %d", issue_part, date, ampm);
+	while (1)
+	{
+		printf("\n午前 : 1\n午後 : 2\n");
+		scanf("%d", &ampm);
+		// Validate int 1-2
+		if (validate_int(1, 2, ampm))
+			break;
+		print_validation_err(ampm);
+	}
+
 	return (0);
+}
+
+int	validate_int(int input_range_beginning, int input_range_end, int input)
+{
+	if (input_range_beginning <= input && input <= input_range_end)
+		return (1); // true
+	return (0); // false
+}
+
+int	validate_date(int range_end, int input)
+{
+	// 現在の日付を元にバリデーションを行う
+	// 休館判定は未考慮
+}
+
+void	print_validation_err(int input)
+{
+	printf("\n%d は対象外の数値です。再度入力してください。\n", input);
 }
--- src/mylib.h
+++ src/mylib.h
@@ -8,5 +8,8 @@
 #include <math.h>
 
 int inquiry(void);
+int validate_int(int input_range_beginning, int input_range_end, int input);
+int validate_date(int range_end, int input);
+void print_validation_err(int input);
 
 #endif
