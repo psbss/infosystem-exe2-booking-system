13b212f78a1afeb6c422e0768d5c0d29a835d337
Yuu <uypsbss@gmail.com>
2020-11-30 16:01:50 +0900

    fix srand / rand

--- src/inquiry_random.c
+++ src/inquiry_random.c
@@ -12,20 +12,20 @@ int	random_inquiry(void){
 
 	// get_rand(rand_start, rand_end)
 	// rand_start < rand_end
-	issue_part = get_rand(1.0, 5.0);
-	date = current_time_int + get_rand(1.0, 7.0);
-	ampm = get_rand(1.0, 2.0);
+	issue_part = get_rand(current_time, 1.0, 5.0);
+	date = current_time_int + get_rand(current_time, 1.0, 7.0);
+	ampm = get_rand(current_time, 1.0, 2.0);
 
 	return (0);
 }
 
-int	get_rand(double rand_start, double rand_end){
+int	get_rand(time_t current_time, double rand_start, double rand_end){
 	// rand() % max を利用した方法は乱数として数値の余りを利用しているため偏りが発生する。
 	// get_rand を利用することで、擬似乱数として本質的に乱数に近い数値を返すことが可能である。
 
-	time_t current_time = time(NULL);
 	// init random
 	srand((unsigned int)current_time);
+	// current_time を関数実行時に変更すると乱数生成が失敗するため、呼び出し元から時間を引き継いで初期化している。
 
-	return (rand_start + (int)((rand_end - rand_start + 1.0) * rand() / (RAND_MAX + 1.0)));
+	return ((int)rand_start + (int)((rand_end - rand_start + 1.0) * rand() / (RAND_MAX + 1.0)));
 }
