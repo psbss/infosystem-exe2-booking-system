15fe80331b218908c847bb3726c8fe638403e320
Yuu <uypsbss@gmail.com>
2020-11-30 14:46:01 +0900

    add get_rand

--- src/inquiry_random.c
+++ src/inquiry_random.c
@@ -6,16 +6,21 @@ int	random_inquiry(void){
 	int ampm;
 	time_t current_time = time(NULL);
 
+	struct tm Time;
+	localtime_r(&current_time, &Time);
+	int current_time_int = (Time.tm_year + 1900) * 10000 + (Time.tm_mon + 1) * 100 + (Time.tm_mday);
+
+	issue_part = get_rand(1.0, 5.0);
+	date = current_time_int + get_rand(1.0, 7.0);
+	ampm = get_rand(1.0, 2.0);
+
+	return (0);
+}
+
+int	get_rand(double rand_start, double rand_end){
+	time_t current_time = time(NULL);
 	// init random
 	srand((unsigned int)current_time);
 
-	struct tm tm;
-	localtime_r(&current_time, &tm);
-	int current_time_int = (tm.tm_year + 1900) * 10000 + (tm.tm_mon + 1) * 100 + (tm.tm_mday);
-
-	issue_part = 1 + rand() % 5;
-	date = current_time_int + (1 + rand() % 7);
-	ampm = 1 + rand() % 2;
-
-	return (0);
+	return (rand_start + (int)((rand_end - rand_start + 1.0) * rand() / (RAND_MAX + 1.0)));
 }
--- src/mylib.h
+++ src/mylib.h
@@ -13,5 +13,6 @@ int validate_int(int input_range_beginning, int input_range_end, int input);
 int validate_date(int range_end, int input);
 void print_validation_err(int input);
 int random_inquiry(void);
+int get_rand(double rand_start, double rand_end);
 
 #endif
