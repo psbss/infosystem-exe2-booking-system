a8093364abefd0b408f552167d5304af6803bb2f
yoh-TKT <72724490+yoh-TKT@users.noreply.github.com>
2020-12-07 14:52:36 +0900

    change variable name

--- src/johnson_algorithm.c
+++ src/johnson_algorithm.c
@@ -1,53 +1,41 @@
-#include "mylib.h"
+int johnson(int* no, int* time1, int* time2) {
 
-struct BODY
-{
-	int time1; //M1
-	int time2; //M2
-};
+	//ポインタで渡されたno配列のコピー
+	int johnson[11];
+	//最小時間
+	int min_time;
+	//最小時間の番号
+	int min_costomer;
+	//1から11人の患者（配列の先頭と末尾）
+	int n, m;
 
-struct CUSTOMER
-{
-	int no; //患部
-};
+	int i, change;
 
+	//johnson配列にno配列の値をコピー
+	for (i = 0; i < 11; i++) {
+		johnson[i] = no[i];
+	}
 
-int johnson(void){
-
-	struct BODY body[4];
-	struct COSTOMER costomer[11], johnson[11], change; //chageはjohnsonの値を保存
-	
-	int min_time; //最小時間
-	int min_costomer;//最小時間の番号
-	int n, m; //1から11人の患者（配列の先頭と末尾）
-	int sumtime1, sumtime2; //M1とM2の合計時間
-	int i;
-
-	//bodyの初期値代入
-
-	//costomerの初期値代入
-
-    //costomerのデータをjohnsonへ移す
 
 	//ジョンソン法
 	n = 0;
 	m = 10;
-	while (m - n > 0){
+	while (m - n > 0) {
 
-		min_time = body[johnson[n].no].time1;
+		min_time = time1[johnson[n]];
 		min_costomer = n + 1;
 
 		for (i = n; i <= m; i++) {
-			if (body[johnson[i].no].time1 <= body[johnson[i].no].time2) {
-				if ((body[johnson[i].no].time1 == min_time && body[johnson[i].no].time2 < body[johnson[abs(min_costomer) - 1].no].time2) || body[johnson[i].no].time1 < min_time) {
-					min_time = body[johnson[i].no].time1;
+			if (time1[johnson[i]] <= time2[johnson[i]]) {
+				if ((time1[johnson[i]] == min_time && time2[johnson[i]] < time2[johnson[abs(min_costomer) - 1]]) || time1[johnson[i]] < min_time) {
+					min_time = time1[johnson[i]];
 					min_costomer = i + 1;
 				}
 			}
 			//else
-			if (body[johnson[i].no].time1 > body[johnson[i].no].time2) {
-				if ((body[johnson[i].no].time1 == min_time && body[johnson[i].no].time1 > body[johnson[abs(min_costomer) - 1].no].time1) || body[johnson[i].no].time2 < min_time) {
-					min_time = body[johnson[i].no].time2;
+			if (time1[johnson[i]] > time2[johnson[i]]) {
+				if ((time1[johnson[i]] == min_time && time1[johnson[i]] > time1[johnson[abs(min_costomer) - 1]]) || time2[johnson[i]] < min_time) {
+					min_time = time2[johnson[i]];
 					min_costomer = -(i + 1);
 				}
 			}
@@ -66,24 +54,6 @@ int johnson(void){
 		}
 	}
 
-	//合計時間（ガントチャート）
-	sumtime1 = 0;
-	
-	for (i = 0; i < 11; i++) {
-		sumtime1 += body[johnson[i].no].time1;
-
-		if (i == 0) {
-			sumtime2 = sumtime1 + body[johnson[i].no].time2;
-		}
-		//else if
-		if (i > 0 && sumtime1 > sumtime2) {
-			sumtime2 += body[johnson[i].no].time2 + (sumtime1 - sumtime2);
-		}
-		//else
-		if (i > 0 && sumtime1 <= sumtime2) {
-			sumtime2 += body[johnson[i].no].time2;
-		}
-	}
-
-	return 0;
+	//戻り値はjohnson配列になる
+	return *johnson;
 }
