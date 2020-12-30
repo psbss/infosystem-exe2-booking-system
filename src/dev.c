9e7f8db9aecc4cce826e9c6d3868cb90698aaf9c
Yuu <uypsbss@gmail.com>
2020-12-31 03:19:43 +0900

    add index number

--- src/dev.c
+++ src/dev.c
@@ -10,6 +10,7 @@ void database_printf(int db_no, int db_type, int database[db_no][db_type]){
 	//  - 診察時間
 	//  - 施術時間
 
+	int ct = 0;
 	int ct_type = 0;
 	int ct_no = 0;
 	char *database_head[6] = {
@@ -33,6 +34,7 @@ void database_printf(int db_no, int db_type, int database[db_no][db_type]){
 	while (ct_no < db_no)
 	{
 		ct_type = 0;
+		printf("%d: ", ct);
 		while (ct_type < db_type)
 		{
 			printf("%d ", database[ct_no][ct_type]);
@@ -40,5 +42,6 @@ void database_printf(int db_no, int db_type, int database[db_no][db_type]){
 		}
 		putchar('\n');
 		ct_no++;
+		ct++;
 	}
 }
