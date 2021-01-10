0758c66fd0a0a15f7ffe6ed662e798a163664215
Yuu <uypsbss@gmail.com>
2021-01-10 15:49:36 +0900

    refactor

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]){
 	int db_thu_pm_no = 0;
 	int db_sat_am_no = 0;
 	int db_sat_pm_no = 0;
+	int result[DATABASE_BLOCK_NUMBER];
 	double_array_init(DATABASE_NUMBER, DATABASE_TYPE, database);
 	double_array_init(DATABASE_BLOCK_NUMBER, DATABASE_TYPE, db_mon_am);
 	double_array_init(DATABASE_BLOCK_NUMBER, DATABASE_TYPE, db_mon_pm);
@@ -28,6 +29,7 @@ int main(int argc, char *argv[]){
 	double_array_init(DATABASE_BLOCK_NUMBER, DATABASE_TYPE, db_thu_pm);
 	double_array_init(DATABASE_BLOCK_NUMBER, DATABASE_TYPE, db_sat_am);
 	double_array_init(DATABASE_BLOCK_NUMBER, DATABASE_TYPE, db_sat_pm);
+	array_init(DATABASE_BLOCK_NUMBER, result);
 
 	printf("array_init finished! \n");
 
@@ -40,11 +42,8 @@ int main(int argc, char *argv[]){
 	}
 
 	add_exeopetime(DATABASE_TYPE, 1, 4, 5, DATABASE_NUMBER, database);
-
 	bubble_sort_double_arr(DATABASE_TYPE, 2, DATABASE_NUMBER, database);
 	separate_arr(DATABASE_TYPE, 2, database, db_mon_am, db_mon_pm, db_tue_am, db_tue_pm, db_thu_am, db_thu_pm, db_sat_am, db_sat_pm);
-	print_double_arr(DATABASE_NUMBER, DATABASE_TYPE, database);
-	print_double_arr(DATABASE_BLOCK_NUMBER, DATABASE_TYPE, db_mon_am);
 
 	db_mon_am_no = arr_len(DATABASE_TYPE, db_mon_am);
 	db_mon_pm_no = arr_len(DATABASE_TYPE, db_mon_pm);
@@ -55,10 +54,8 @@ int main(int argc, char *argv[]){
 	db_sat_am_no = arr_len(DATABASE_TYPE, db_sat_am);
 	db_sat_pm_no = arr_len(DATABASE_TYPE, db_sat_pm);
 
-	int sum[8];
 	int arr[DATABASE_BLOCK_NUMBER];
 	int arr2[DATABASE_BLOCK_NUMBER];
-	array_init(8, sum);
 	array_init(DATABASE_BLOCK_NUMBER, arr);	
 	array_init(DATABASE_BLOCK_NUMBER, arr2);
 
@@ -69,12 +66,9 @@ int main(int argc, char *argv[]){
 		arr2[i] = i;
 	}
 
-	johnson(db_mon_am, 0, db_mon_am_no, arr2, sum);
+	johnson(db_mon_am, 0, db_mon_am_no, arr2, result);
+	print_result(db_mon_am_no, arr2);
 
-	for (int i = 0; i < db_mon_am_no; i++)
-	{
-		printf("%d -> ", arr2[i]);
- 	}
-
+	printf("program finished!");
 	return (0);
 }
