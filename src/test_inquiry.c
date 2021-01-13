e34ccfc7edddb2a99f72d8e385a67f4b9f562e0a
Yuu <uypsbss@gmail.com>
2021-01-13 16:40:30 +0900

    add test

--- src/test_inquiry.c
+++ src/test_inquiry.c
@@ -8,5 +8,20 @@
 //  - 診察時間
 //  - 施術時間
 void test_inquiry(int no, int types, int db[][types]){
-
+	int issue = 0;
+	int date = get_time_now();
+	int ampm = 1;
+	for (int i = 0; i < no; i++)
+	{
+		if(issue == 0)
+			date++;
+		db[i][0] = i;
+		db[i][1] = issue++;
+		if(issue == 6)
+			issue = 0;
+		db[i][2] = date;
+		db[i][3] = ampm++;
+		if(ampm == 3)
+			ampm = 1;
+	}
 }
